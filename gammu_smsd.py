f4b952dc642d8907285bac45e368012cd2a6367c
Nsoids <92770069+Nsoids@users.noreply.github.com>
2023-01-06 16:07:18 +0800

    Create gammu_smsd.py

---
+++ gammu_smsd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import requests
+
+numparts = int(os.environ["DECODED_PARTS"])
+
+text = ""
+# Are there any decoded parts?
+if numparts == 0:
+    text = os.environ["SMS_1_TEXT"]
+# Get all text parts
+else:
+    for i in range(1, numparts + 1):
+        varname = "DECODED_%d_TEXT" % i
+        if varname in os.environ:
+            text = text + os.environ[varname]
+
+# Set the webhook URL and the data to be sent
+webhook_url = "https://hooks.slack.com/services/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"
+message = text
+string = "FROM: "
+number = os.environ["SMS_1_NUMBER"]
+
+data = {
+        "text": string + str(number),
+    "attachments": [
+        {
+            "text": message,
+            "color": "#36a64f"
+        }
+    ]
+}
+
+# Send a POST request to the webhook with the data
+response = requests.post(webhook_url, json=data)
